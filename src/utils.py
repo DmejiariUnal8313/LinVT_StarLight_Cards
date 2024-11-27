81907dbb2fec30e68247cb5a112a81384375fec0
Daniel Felipe Mejía Ríos <dmejiari@unal.edu.co>
2024-11-27 01:23:39 -0500

    ajustar la carga de imágenes para compatibilidad con PyInstaller

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,9 @@
 import pygame
+import os
+import sys
 
 def load_image(path):
+    # Ajustar la ruta para PyInstaller
+    if hasattr(sys, '_MEIPASS'):
+        path = os.path.join(sys._MEIPASS, path)
     return pygame.image.load(path).convert_alpha()
